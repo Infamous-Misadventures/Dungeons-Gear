375dbc48a98a2f5373ec71051bae1afcbd82a4cd
Mike Garay <meg89@duke.edu>
2020-09-15 21:15:51 -0500

    Fixed issue with server crash due to PlayerRendererMixin being loaded on the server instead of only in the client

--- src/main/java/com/infamous/dungeons_gear/mixin/PlayerRendererMixin.java
+++ src/main/java/com/infamous/dungeons_gear/mixin/PlayerRendererMixin.java
@@ -5,7 +5,6 @@ import net.minecraft.client.renderer.entity.PlayerRenderer;
 import net.minecraft.client.renderer.entity.model.BipedModel;
 import net.minecraft.item.CrossbowItem;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.util.Hand;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
