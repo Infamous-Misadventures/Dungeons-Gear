3d01228e17e3805f9f6fe14ea9f987213b76073e
jackiecrazy <jackiecrazyhi@gmail.com>
2021-05-20 11:49:03 +0800

    pehkui fix

--- src/main/java/com/infamous/dungeons_gear/mixin/GameRendererMixin.java
+++ src/main/java/com/infamous/dungeons_gear/mixin/GameRendererMixin.java
@@ -3,42 +3,60 @@ package com.infamous.dungeons_gear.mixin;
 import com.infamous.dungeons_gear.init.AttributeRegistry;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.vector.Vector3d;
 import net.minecraftforge.common.ForgeMod;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(GameRenderer.class)
 public abstract class GameRendererMixin {
     @Shadow
     private Minecraft mc;
 
-    @ModifyConstant(
-            method = "getMouseOver",
-            constant = @Constant(doubleValue = 6.0D)
-    )
-    private double getExtendedAttackReach(double value) {
-        assert mc.player != null;
-        return mc.player.getAttributeValue(AttributeRegistry.ATTACK_REACH.get()) + 3.0D;
+    @Redirect(method = "getMouseOver", at = @At(value = "INVOKE", ordinal = 0, target = "Lnet/minecraft/util/math/vector/Vector3d;squareDistanceTo(Lnet/minecraft/util/math/vector/Vector3d;)D"))
+    private double getModifiedDistance1(Vector3d vector3d, Vector3d vec) {
+        PlayerEntity playerEntity=Minecraft.getInstance().player;
+        double reach = playerEntity.getAttributeValue(AttributeRegistry.ATTACK_REACH.get()) - 3;
+        return vector3d.squareDistanceTo(vec) + (6 * reach + reach * reach);
+    }
+@Redirect(method = "getMouseOver", at = @At(value = "INVOKE", ordinal = 1, target = "Lnet/minecraft/util/math/vector/Vector3d;squareDistanceTo(Lnet/minecraft/util/math/vector/Vector3d;)D"))
+    private double getModifiedDistance2(Vector3d vector3d, Vector3d vec) {
+        PlayerEntity playerEntity=Minecraft.getInstance().player;
+        double reach = playerEntity.getAttributeValue(AttributeRegistry.ATTACK_REACH.get()) - 3;
+        return vector3d.squareDistanceTo(vec) - (6 * reach + reach * reach);
     }
 
-    @ModifyConstant(
-            method = "getMouseOver",
-            constant = @Constant(doubleValue = 3.0D)
-    )
-    private double getAttackReach(double value) {
-        assert mc.player != null;
-        return mc.player.getAttributeValue(AttributeRegistry.ATTACK_REACH.get());
-    }
-
-    @ModifyConstant(
-            method = "getMouseOver",
-            constant = @Constant(doubleValue = 9.0D)
-    )
-    private double getAttackReachSquared(double value) {
-        assert mc.player != null;
-        double attackReachValue = mc.player.getAttributeValue(AttributeRegistry.ATTACK_REACH.get());
-        return attackReachValue * attackReachValue;
-    }
+//    @ModifyConstant(
+//            method = "getMouseOver",
+//            constant = @Constant(doubleValue = 6.0D)
+//    )
+//    private double getExtendedAttackReach(double value) {
+//        assert mc.player != null;
+//        return mc.player.getAttributeValue(AttributeRegistry.ATTACK_REACH.get()) + 3.0D;
+//    }
+//
+//    @ModifyConstant(
+//            method = "getMouseOver",
+//            constant = @Constant(doubleValue = 3.0D)
+//    )
+//    private double getAttackReach(double value) {
+//        assert mc.player != null;
+//        return mc.player.getAttributeValue(AttributeRegistry.ATTACK_REACH.get());
+//    }
+//
+//    @ModifyConstant(
+//            method = "getMouseOver",
+//            constant = @Constant(doubleValue = 9.0D)
+//    )
+//    private double getAttackReachSquared(double value) {
+//        assert mc.player != null;
+//        double attackReachValue = mc.player.getAttributeValue(AttributeRegistry.ATTACK_REACH.get());
+//        return attackReachValue * attackReachValue;
+//    }
 }
