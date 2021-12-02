a08da5ac282ced42822a2751635171aa131a2717
Patrigan <patrick.provinciael@gmail.com>
2021-12-02 20:15:20 +0100

    Fix name issues.

--- src/main/java/com/infamous/dungeons_gear/items/armor/EmeraldGearArmorItem.java
+++ src/main/java/com/infamous/dungeons_gear/items/armor/EmeraldGearArmorItem.java
@@ -1,5 +1,7 @@
 package com.infamous.dungeons_gear.items.armor;
 
+import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.Multimap;
 import com.infamous.dungeons_gear.DungeonsGear;
 import com.infamous.dungeons_gear.items.armor.models.old_models.*;
 import com.infamous.dungeons_gear.items.interfaces.IArmor;
@@ -9,6 +11,9 @@ import net.minecraft.client.renderer.entity.model.BipedModel;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.ai.attributes.Attribute;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.IArmorMaterial;
@@ -20,13 +25,25 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
 import java.util.List;
+import java.util.UUID;
 
 public class EmeraldGearArmorItem extends ArmorItem implements IArmor {
     private final boolean unique;
+    private static final UUID[] ARMOR_MODIFIERS = new UUID[]{
+            UUID.fromString("845DB27C-C624-495F-8C9F-6020A9A58B6B"),
+            UUID.fromString("D8499B04-0E66-4726-AB29-64469D734E0D"),
+            UUID.fromString("9F3D476D-C118-4544-8365-64846904B48E"),
+            UUID.fromString("2AD3F246-FEE1-4E67-B886-69FD380BB150")};
+    private final Multimap<Attribute, AttributeModifier> attributeModifiers;
 
     public EmeraldGearArmorItem(IArmorMaterial armorMaterial, EquipmentSlotType slotType, Properties properties, boolean unique) {
         super(armorMaterial, slotType, properties);
         this.unique = unique;
+
+        ImmutableMultimap.Builder<Attribute, AttributeModifier> builder = ImmutableMultimap.builder();
+        UUID uuid = ARMOR_MODIFIERS[slot.getIndex()];
+            builder.put(Attributes.ATTACK_SPEED, new AttributeModifier(uuid, "Armor attack speed boost", 0.25D * 0.5D, AttributeModifier.Operation.MULTIPLY_BASE));
+        this.attributeModifiers = builder.build();
     }
 
 
@@ -58,6 +75,11 @@ public class EmeraldGearArmorItem extends ArmorItem implements IArmor {
             return (A) new OpulentArmorModel<>(1.0F, slot, entityLiving);
         }
         return null;
+    }
+
+    @Override
+    public Multimap<Attribute, AttributeModifier> getDefaultAttributeModifiers(EquipmentSlotType equipmentSlot) {
+        return equipmentSlot == this.slot ? this.attributeModifiers : super.getDefaultAttributeModifiers(equipmentSlot);
     }
 
 
