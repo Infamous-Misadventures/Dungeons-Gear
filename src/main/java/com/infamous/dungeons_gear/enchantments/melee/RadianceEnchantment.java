162228777d83d85f4e5d73fbea08ff17e3d610d0
Patrigan <patrick.provinciael@gmail.com>
2022-06-25 17:39:40 +0200

    Radiance is Healing Enchantment

--- src/main/java/com/infamous/dungeons_gear/enchantments/melee/RadianceEnchantment.java
+++ src/main/java/com/infamous/dungeons_gear/enchantments/melee/RadianceEnchantment.java
@@ -1,14 +1,17 @@
 package com.infamous.dungeons_gear.enchantments.melee;
 
+import com.infamous.dungeons_gear.config.DungeonsGearConfig;
 import com.infamous.dungeons_gear.damagesources.OffhandAttackDamageSource;
 import com.infamous.dungeons_gear.enchantments.ModEnchantmentTypes;
 import com.infamous.dungeons_gear.enchantments.lists.MeleeEnchantmentList;
 import com.infamous.dungeons_gear.enchantments.types.DungeonsEnchantment;
+import com.infamous.dungeons_gear.enchantments.types.HealingEnchantment;
 import com.infamous.dungeons_libraries.items.interfaces.IMeleeWeapon;
 import com.infamous.dungeons_gear.utilties.AOECloudHelper;
 import com.infamous.dungeons_gear.utilties.AreaOfEffectHelper;
 import com.infamous.dungeons_gear.utilties.ModEnchantmentHelper;
 import com.infamous.dungeons_gear.utilties.PlayerAttackHelper;
+import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
@@ -35,6 +38,11 @@ public class RadianceEnchantment extends DungeonsEnchantment {
 
     public int getMaxLevel() {
         return 3;
+    }
+
+    @Override
+    public boolean checkCompatibility(Enchantment enchantment) {
+        return DungeonsGearConfig.ENABLE_OVERPOWERED_ENCHANTMENT_COMBOS.get() || !(enchantment instanceof HealingEnchantment);
     }
 
     @Override
