d4e93d3affc7ebd585fd14346f547c5fe6c93163
Patrigan <patrick.provinciael@gmail.com>
2022-06-26 22:36:55 +0200

    Fix HarpoonShotEnchantment

--- src/main/java/com/infamous/dungeons_gear/enchantments/ranged/HarpoonShotEnchantment.java
+++ src/main/java/com/infamous/dungeons_gear/enchantments/ranged/HarpoonShotEnchantment.java
@@ -1,14 +1,18 @@
 package com.infamous.dungeons_gear.enchantments.ranged;
 
+import com.infamous.dungeons_gear.capabilities.combo.ICombo;
 import com.infamous.dungeons_gear.enchantments.ModEnchantmentTypes;
 import com.infamous.dungeons_gear.enchantments.lists.RangedEnchantmentList;
 import com.infamous.dungeons_gear.enchantments.types.DungeonsEnchantment;
-import com.infamous.dungeons_gear.utilties.AOECloudHelper;
-import com.infamous.dungeons_gear.utilties.AreaOfEffectHelper;
-import com.infamous.dungeons_gear.utilties.ModEnchantmentHelper;
-import com.infamous.dungeons_gear.utilties.SoundHelper;
+import com.infamous.dungeons_gear.items.artifacts.HarpoonQuiverItem;
+import com.infamous.dungeons_gear.items.artifacts.ThunderingQuiverItem;
+import com.infamous.dungeons_gear.items.artifacts.TormentQuiverItem;
+import com.infamous.dungeons_gear.utilties.*;
 import com.infamous.dungeons_libraries.utils.ArrowHelper;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.AbstractArrowEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.util.IndirectEntityDamageSource;
@@ -16,6 +20,7 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.RayTraceResult;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.entity.ProjectileImpactEvent;
 import net.minecraftforge.event.entity.living.LivingDamageEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -34,5 +39,19 @@ public class HarpoonShotEnchantment extends DungeonsEnchantment {
     @Override
     public int getMaxLevel() {
         return 1;
+    }
+
+    @SubscribeEvent
+    public static void onArrowJoinWorld(EntityJoinWorldEvent event){
+        if(event.getEntity() instanceof AbstractArrowEntity && !event.getWorld().isClientSide()){
+            AbstractArrowEntity arrowEntity = (AbstractArrowEntity) event.getEntity();
+            if(arrowEntity.getOwner() instanceof LivingEntity){
+                LivingEntity livingEntity = (LivingEntity) arrowEntity.getOwner();
+                EnchantmentHelper.getEnchantmentLevel(RangedEnchantmentList.HARPOON_SHOT, livingEntity);
+                arrowEntity.addTag(HarpoonQuiverItem.HARPOON_QUIVER);
+                arrowEntity.setDeltaMovement(arrowEntity.getDeltaMovement().scale(1.5D));
+                arrowEntity.setPierceLevel((byte) (arrowEntity.getPierceLevel() + 1));
+            }
+        }
     }
 }
--- src/main/java/com/infamous/dungeons_gear/items/artifacts/ArtifactEvents.java
+++ src/main/java/com/infamous/dungeons_gear/items/artifacts/ArtifactEvents.java
@@ -123,6 +123,7 @@ public class ArtifactEvents {
                     count--;
                     comboCap.setHarpoonCount(count);
                     arrowEntity.setDeltaMovement(arrowEntity.getDeltaMovement().scale(1.5D));
+                    arrowEntity.setPierceLevel((byte) (arrowEntity.getPierceLevel() + 1));
                 }
             }
         }
