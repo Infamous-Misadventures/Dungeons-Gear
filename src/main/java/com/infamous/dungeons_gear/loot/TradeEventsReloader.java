4fcf2e0ecafff33b491128a8c19fb8216137aa47
Patrigan <patrick.provinciael@gmail.com>
2022-06-26 22:20:56 +0200

    Fix trader reloader and Abnormals Core incompat

--- src/main/java/com/infamous/dungeons_gear/loot/TradeEventsReloader.java
+++ src/main/java/com/infamous/dungeons_gear/loot/TradeEventsReloader.java
@@ -7,18 +7,22 @@ import net.minecraft.resources.IResourceManagerReloadListener;
 import net.minecraftforge.event.AddReloadListenerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.resource.IResourceType;
+import net.minecraftforge.resource.ISelectiveResourceReloadListener;
+
+import java.util.function.Predicate;
 
 @Mod.EventBusSubscriber(modid = DungeonsGear.MODID)
-public class TradeEventsReloader implements IResourceManagerReloadListener {
+public class TradeEventsReloader implements ISelectiveResourceReloadListener {
 
 
     @SubscribeEvent
     public static void onAddReloadListeners(AddReloadListenerEvent event) {
-        event.addListener(new TradeEventsReloader());
+//        event.addListener(new TradeEventsReloader());
     }
 
     @Override
-    public void onResourceManagerReload(IResourceManager p_195410_1_) {
+    public void onResourceManagerReload(IResourceManager p_195410_1_, Predicate<IResourceType> resourcePredicate) {
         DungeonsGear.LOGGER.info("Beginning retriggering trader events");
         VillagerTradingManagerAccessor.invokePostWandererEvent();
         VillagerTradingManagerAccessor.invokePostVillagerEvents();
