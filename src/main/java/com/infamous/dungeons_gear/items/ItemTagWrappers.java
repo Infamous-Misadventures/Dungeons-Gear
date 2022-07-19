e5e2daef13650b1d71ff0d9b6d31920551c18edb
pprovinciael <patrick.provinciael@dpgmedia.be>
2022-07-19 18:24:27 +0200

    Github Issue #161 Add forge tagging

--- src/main/java/com/infamous/dungeons_gear/items/ItemTagWrappers.java
+++ src/main/java/com/infamous/dungeons_gear/items/ItemTagWrappers.java
@@ -18,6 +18,8 @@ public class ItemTagWrappers {
     public static final Tags.IOptionalNamedTag<Item> FOOD = ItemTags.createOptional(new ResourceLocation(MOD_ID, "food"));
     public static final Tags.IOptionalNamedTag<Item> FOOD_PROCESSED = ItemTags.createOptional(new ResourceLocation(MOD_ID, "food/processed"));
     public static final Tags.IOptionalNamedTag<Item> FOOD_RAW = ItemTags.createOptional(new ResourceLocation(MOD_ID, "food/raw"));
+    public static final Tags.IOptionalNamedTag<Item> WEAPONS = ItemTags.createOptional(new ResourceLocation(MOD_ID, "weapons"));
+    public static final Tags.IOptionalNamedTag<Item> ARMOR = ItemTags.createOptional(new ResourceLocation(MOD_ID, "armor"));
 
     public static final Tags.IOptionalNamedTag<Item> CURIOS_ARTIFACTS = ItemTags.createOptional(new ResourceLocation("curios", "artifact"));
 }
