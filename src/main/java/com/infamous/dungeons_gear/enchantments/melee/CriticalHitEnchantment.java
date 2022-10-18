19af738e2e85725767ebc8f37049a3ef2bb9cc96
Patrigan <patrick.provinciael@gmail.com>
2022-10-18 16:44:05 +0200

    Fix Critical Hit and Chains

--- src/main/java/com/infamous/dungeons_gear/enchantments/melee/CriticalHitEnchantment.java
+++ src/main/java/com/infamous/dungeons_gear/enchantments/melee/CriticalHitEnchantment.java
@@ -36,7 +36,6 @@ public class CriticalHitEnchantment extends DamageBoostEnchantment {
             PlayerEntity attacker = event.getPlayer();
             ItemStack mainhand = attacker.getMainHandItem();
             boolean success = false;
-            if (event.getResult() != Event.Result.ALLOW && mainhand.getItem() instanceof IComboWeapon) return;
             if (ModEnchantmentHelper.hasEnchantment(mainhand, MeleeEnchantmentList.CRITICAL_HIT)) {
                 int criticalHitLevel = EnchantmentHelper.getItemEnchantmentLevel(MeleeEnchantmentList.CRITICAL_HIT, mainhand);
                 float criticalHitChance;
