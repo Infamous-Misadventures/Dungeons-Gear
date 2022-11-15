565bb4f4d3628636a694ebb0d9e10b374d62120a
Patrigan <patrick.provinciael@gmail.com>
2022-11-15 21:35:11 +0100

    Fixes to Love Medallion ported to 1.18

--- src/main/java/com/infamous/dungeons_gear/items/artifacts/LoveMedallionItem.java
+++ src/main/java/com/infamous/dungeons_gear/items/artifacts/LoveMedallionItem.java
@@ -55,6 +55,8 @@ public class LoveMedallionItem extends ArtifactItem {
             minionCapability.setTemporary(true);
             minionCapability.setRevertsOnExpiration(true);
             minionCapability.setMinionTimer(200);
+            minionCapability.setGoalsAdded(false);
+            ((Monster) nearbyEntity).setTarget(null);
             MinionMasterHelper.addMinionGoals(mobEntity);
         }
     }
